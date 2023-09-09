df17c253fed3276e57b55a4d33e9a7816df1ed44
aya-cs22 <aya786930@gmail.com>
2023-09-09 04:36:58 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,12 @@ int main(void)
 {
 	char ch = 'a';
 
-	if (ch != 'e' && ch != 'q')
+	while (ch <= 'z')
 	{
-		while (ch <= 'z')
-		{
-			putchar(ch);
+		if (ch == 'e' || ch == 'q')
 			ch++;
-		}
+		putchar(ch);
+		ch++;
 	}
 	putchar('\n');
 	return (0);
