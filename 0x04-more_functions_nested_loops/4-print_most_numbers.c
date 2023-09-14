2206c24e6df1cfaf5e2ee395d7605c2db32943b4
aya-cs22 <aya786930@gmail.com>
2023-09-13 22:19:00 -0700

    my_file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * print_most_numbers - prints the numbers, from 0 to 9 without print 2 or 4
+ * Return: 0 (success)
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar (i + 48);
+		i++;
+		if (i == 2 || i == 4)
+			i++;
+	}
+	_putchar('\n');
+}
