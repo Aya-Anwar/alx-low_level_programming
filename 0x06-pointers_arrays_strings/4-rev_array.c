5e97d52594d5f1c0996a34f533d6bc86572759df
aya-cs22 <aya786930@gmail.com>
2023-09-22 21:55:15 -0700

    my_file

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+/**
+ * reverse_array -  reverses the content of an array of integers
+ * @a: An array of integers
+ * @n: The number of elements to swap
+ * Return: Nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, swap;
+
+	for (i = 0, j = (n - 1); i < j; i++, j--)
+	{
+		swap = a[i];
+		a[i] = a[j];
+		a[j] = swap;
+	}
+}
