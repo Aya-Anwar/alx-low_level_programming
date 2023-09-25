a49efd62258c78c377533b9cba74762c8a8891ec
aya-cs22 <aya786930@gmail.com>
2023-09-25 02:20:09 -0700

    my_file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,6 +4,7 @@
  * @s: A pointer to the memory block you want to set
  * @b: The value you want to set. It's usually an integer
  * @n: The number of bytes you want to set to the specified value
+ * Return: char with result of memset
  */
 char *_memset(char *s, char b, unsigned int n)
 {
