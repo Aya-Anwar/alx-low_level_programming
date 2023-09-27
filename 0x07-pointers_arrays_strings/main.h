eed71b2cc17d5569b3f499f04e5be6b7dbe48ff7
aya-cs22 <aya786930@gmail.com>
2023-09-27 02:17:13 -0700

    my_file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,4 +9,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 void set_string(char **s, char *to);
 char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
 #endif
