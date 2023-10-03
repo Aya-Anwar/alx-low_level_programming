767846e77d779d458047604144cbb381539bf37c
aya-cs22 <aya786930@gmail.com>
2023-10-03 07:18:52 -0700

    my_file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,14 +9,21 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 1)
+	int i;
+	int sum = 0;
+
+	if (argc == 1)
 	{
-		printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
+		printf("0");
+		return (1);
 	}
 	else
 	{
-		printf("0\n");
-		return (1);
+		for (i = 1; i < argc; i++)
+		{
+		sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
 	}
 	return (0);
 }
