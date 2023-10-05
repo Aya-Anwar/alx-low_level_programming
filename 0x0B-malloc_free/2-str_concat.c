b170ab66abdca38ac8689175f143de2909d1c96b
aya-cs22 <aya786930@gmail.com>
2023-10-05 04:46:14 -0700

    my_file

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * str_concat - Concatenate two strings
+ *
+ * @s1: A string to be concatenated
+ *
+ * @s2: A string to be concatenated
+ *
+ * Return: Pointer to newly allocated memory
+ * which is equal to @s1 + @s2 (SUCCESS) OR
+ * NULL (FAILURE)
+ */
+char *str_concat(char *s1, char *s2)
+{
+	size_t len1;
+	size_t len2;
+	size_t total_len;
+	char *result;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	total_len = len1 + len2 + 1;
+	result = (char *)malloc(total_len);
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(result, s1);
+	strcat(result, s2);
+	return (result);
+}
