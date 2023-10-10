e3110ca3f0bdbad117a539e44039e01c4d2279ba
Aya-Anwar <aya786930@gmail.com>
2023-10-10 11:41:51 -0700

    my_file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -27,8 +27,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_Dog->owner = strdup(owner);
 	if (new_Dog->name == NULL || new_Dog->owner == NULL)
 	{
-		free(new_Dog->name);
-		free(new_Dog->owner);
 		free(new_Dog);
 		return (NULL);
 	}
