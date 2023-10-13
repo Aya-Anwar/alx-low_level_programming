2a5ff6d1b33d828263d5d16a598038bb2aa6322e
Aya-Anwar <aya786930@gmail.com>
2023-10-13 01:26:10 -0700

    my_file

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,70 @@
+#include "variadic_functions.h"
+/**
+ * print_all - Entry Point
+ * c = char, i = int, f = float, s = char * (if null print (nil))
+ * @format: list of arg types
+ * Return: 0
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int num;
+	char ch;
+	float fl;
+	char *str;
+	int j = 0;
+	int string_printed = 0;
+
+	va_start(args, format);
+	for (j = 0; format[j]; j++)
+	{
+		if (format[j] == 'c')
+		{
+			ch = va_arg(args, int);
+			printf("%c", ch);
+			if (format[j + 1] != '\0')
+			{
+				printf(", ");
+			}
+		}
+		else if (format[j] == 'i')
+		{
+			num = va_arg(args, int);
+			printf("%d", num);
+			if (format[j + 1] != '\0')
+			{
+				printf(", ");
+			}
+		}
+		else if (format[j] == 'f')
+		{
+			fl =(float) va_arg(args, double);
+			if (format[j + 1] != '\0')
+			{
+				printf(", ");
+			}
+		}
+		else if (format[j] == 's')
+		{
+			str = va_arg(args, char *);
+			if (str == NULL)
+			{
+				printf("nil");
+			}
+			else
+			{
+				printf("%s", str);
+				if (format[j + 1] != '\0')
+				{
+					printf(", ");
+				}
+				string_printed = 1;
+			}
+		}
+		va_end(args);
+		if (string_printed == 1)
+		{
+			printf("\n");
+		}
+	}
+}
