c4648c54e4284b318dadc0542bfc4a8df4ad73df
aya-cs22 <aya786930@gmail.com>
2023-10-23 16:45:39 +0300

    my_file

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,15 +8,13 @@
  */
 void free_list(list_t *head)
 {
-list_t *temp;
 while (head != NULL)
 {
-temp = head;
-if (temp != NULL)
+if (head != NULL)
 {
-if (temp->str != NULL)
-free(temp->str);
-free(temp);
+if (head->str != NULL)
+free(head->str);
+free(head);
 }
 head = head->next;
 }
