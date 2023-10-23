041bebf394316f5feea5c8baf7096504b3953fe6
aya-cs22 <aya786930@gmail.com>
2023-10-23 16:30:23 +0300

    my_file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees all nodes of a list
+ * @head: pointer to head node
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+list_t *temp = head;
+while (temp != NULL)
+{
+if (temp->str != NULL)
+{
+free(temp->str);
+free(temp);
+}
+}
+}
