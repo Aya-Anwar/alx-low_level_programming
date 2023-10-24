9f9ccd8bce856f4eb26c03476d6c2a92db70b3ae
aya-cs22 <aya786930@gmail.com>
2023-10-24 14:55:14 +0300

    my_file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include"lists.h"
+/**
+ * print_listint - prints a linked lists
+ * @h: pointer to first node
+ * Return: size of list
+*/
+size_t print_listint(const listint_t *h)
+{
+size_t count;
+const listint_t *temp =  h;
+if (temp != NULL)
+{
+for (count = 0; temp != NULL; count++)
+{
+printf("%d\n", temp->n);
+temp = temp->next;
+}
+}
+return (count);
+}
