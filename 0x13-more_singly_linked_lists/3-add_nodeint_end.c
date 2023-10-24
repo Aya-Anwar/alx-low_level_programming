bd33385caf9ed1eba99c6e59a42009b062440ba1
aya-cs22 <aya786930@gmail.com>
2023-10-24 18:18:47 +0300

    my_file

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a node to the bottom
+ * @head: pointer to first node
+ * @n: value for new node
+ *
+ * Return: pointer to new node
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+listint_t *temp = *head;
+listint_t *newnode = malloc(sizeof(listint_t));
+if (newnode == NULL)
+{
+return (NULL);
+}
+newnode->n = n;
+newnode->next = NULL;
+if (temp == NULL)
+{
+*head = newnode;
+return (newnode);
+}
+while (temp->n != NULL)
+{
+temp = temp->n;
+}
+temp->next = newnode;
+return (newnode);
+}
