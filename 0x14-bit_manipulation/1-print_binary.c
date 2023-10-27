36667fb4947a27c60246f38742a7c5e6f6039036
aya-cs22 <aya786930@gmail.com>
2023-10-27 16:06:19 +0200

    my_file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,24 +4,12 @@
  * @n: the number to print
  * Return: void
 */
-void print_binary(unsigned long int n)
-{
-#include <stdio.h>
+void print_binary(unsigned long int n) {
+int numBits = sizeof(n) * 8; // Assuming 8 bits per byte
 
-int binary[32];
-int i = 0;
-int j;
-while (n > 0)
-{
-binary[i] = n % 2;
-n = n / 2;
-i++;
+for (int i = numBits - 1; i >= 0; i--) {
+unsigned long int mask = 1UL << i;
+putchar((n & mask) ? '1' : '0');
 }
-
-for (j = i - 1; j >= 0; j--)
-{
-printf("%d", binary[j]);
-}
-
-return;
+putchar('\n');
 }
