9da38eb223225eacda0cf6d0c3d44cab97d6fd9f
aya-cs22 <aya786930@gmail.com>
2023-10-27 16:11:39 +0200

    my_file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,14 +6,14 @@
 */
 void print_binary(unsigned long int n)
 {
-if (n == 0) {
-_putchar('0');
-return;
-}
 int binary[32];
 int i = 0;
 int j;
 
+if (n == 0) {
+_putchar('0');
+return;
+}
 while (n > 0)
 {
 binary[i] = n % 2;
