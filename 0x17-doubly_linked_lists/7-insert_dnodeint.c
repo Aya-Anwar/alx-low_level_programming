4005194827ea9be53a69ac7c95aeddffea488038
aya-cs22 <aya786930@gmail.com>
2023-12-11 21:06:44 +0200

    a function that inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,47 +10,33 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    dlistint_t *temp = *h;
-    unsigned int i;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *temp = *h;
+	unsigned int i;
 
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
-
-    new_node->n = n;
-
-    if (idx == 0)
-    {
-        new_node->next = *h;
-        new_node->prev = NULL;
-        if (*h != NULL)
-            (*h)->prev = new_node;
-        *h = new_node;
-        return (new_node);
-    }
-
-    for (i = 0; i < idx - 1 && temp != NULL; i++)
-    {
-        temp = temp->next;
-    }
-
-    if (temp == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
-
-    new_node->next = temp->next;
-    new_node->prev = temp;
-    
-    if (temp->next != NULL)
-    {
-        temp->next->prev = new_node;
-    }
-
-    temp->next = new_node;
-
-    return (new_node);
-}
+	if (new_node == NULL)
+	{
+		return (NULL); }
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		new_node->prev = NULL;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node); }
+	for (i = 0; i < idx - 1 && temp != NULL; i++)
+	{
+		temp = temp->next; }
+	if (temp == NULL)
+	{
+		free(new_node);
+		return (NULL); }
+	new_node->next = temp->next;
+	new_node->prev = temp;
+	if (temp->next != NULL)
+	{
+		temp->next->prev = new_node; }
+	temp->next = new_node;
+	return (new_node); }
