01102680bd93aa1f1dba05bf1825c9b22e0d8132
aya-cs22 <aya786930@gmail.com>
2024-01-17 23:28:37 +0200

    Hash tables

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -23,7 +23,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (flag)
 				printf(", ");
-			printf("%s: %s", current->key, current->value);
+			printf("'%s': '%s'", current->key, current->value);
 			flag = 1;
 			current = current->next;
 		}
